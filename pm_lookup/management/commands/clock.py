8a7ec1ae2606b887fac4060b966b60fbe5dedec1
tommasosansone91 <tommaso.sansone@mail.polimi.it>
2020-05-16 19:36:01 +0200

    try to impement scheduled job

--- pm_lookup/management/commands/clock.py
+++ pm_lookup/management/commands/clock.py
@@ -3,6 +3,8 @@ from django.core.management.base import BaseCommand
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+from pm_lookup.processing.scheduled_processing import save_history_pm
+
 # quando scrivo
 # python manage.py calcolo1
 # la funzione command viene rannata automaticamente
@@ -14,11 +16,11 @@ class Command(BaseCommand):
         sched = BlockingScheduler()
         sched.start()
 
-        @sched.interval_schedule(minutes=1)
+        @sched.interval_schedule(seconds=10)
         def job_function():
-            print ("Hello World")
+            save_history_pm()
 
-        
+
         
 
         # Schedule job_function to be called every two hours
