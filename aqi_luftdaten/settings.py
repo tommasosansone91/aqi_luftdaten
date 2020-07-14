73d4a97923c5532f607d9b9909e579c46fb0b6ca
tommasosansone91 <tommaso.sansone@mail.polimi.it>
2020-07-14 12:20:50 +0200

    aumentato limite di linee massime elaborabili per django import-export

--- aqi_luftdaten/settings.py
+++ aqi_luftdaten/settings.py
@@ -148,5 +148,6 @@ django_heroku.settings(locals())  ###
 
 #aggiunto per far funzionare il modulo di importazione csv in admin
 IMPORT_EXPORT_USE_TRANSACTIONS = True
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
 
 
