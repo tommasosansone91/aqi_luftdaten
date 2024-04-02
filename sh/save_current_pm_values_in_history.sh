9b902e9ef66fa3e6dceacad532adb8bcf3600a36
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-04-02 23:36:24 +0200

    added comments

--- sh/save_current_pm_values_in_history.sh
+++ sh/save_current_pm_values_in_history.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# this file will be triggered by the cron and launch the BaseCommand to store data into the database.
+
 cd /var/www/aqi_luftdaten
 
 # source venv/bin/activate
+# it is not needed since I specify the python interpreter from the venv in the next line
 
 venv/bin/python manage.py save_current_pm_values_in_history
