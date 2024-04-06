2a1636a9068166d0f06e0204abae1d7434145860
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-04-06 22:19:58 +0200

    remove call to the scripts which rearranges the time series and graphs

--- pm_lookup/management/commands/save_current_pm_values_in_history.py
+++ pm_lookup/management/commands/save_current_pm_values_in_history.py
@@ -1,7 +1,3 @@
-# creo uno script per poterlo usare nell'heroku scheduler
-# ne faccio un base command, così ogni tot heroku lo ranna ed è come se lo runnasse da consolle
-
-
 from django.core.management.base import BaseCommand
 
 from pm_lookup.processing.scheduled_processing import save_history_pm
@@ -15,9 +11,7 @@ from pm_lookup.processing.scheduled_processing_2 import arrange_time_series_and_
 class Command(BaseCommand):
     def handle(self, *args, **options):
         
-        #salva i valori nel modello storico orario
+        #salva i valori nel modello storico grezzo
         save_history_pm()
         
-        #arrangia le serie orarie attingendo al modello storico orario
-        arrange_time_series_and_graphs()
-        
+
