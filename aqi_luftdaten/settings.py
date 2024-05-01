74b803912c527d3c46c2015b7e308f557491d7b2
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-05-01 12:51:48 +0200

    changed STATIC_ROOT : last folder changed from staticfiles to static to be the same of STATIC_URL

--- aqi_luftdaten/settings.py
+++ aqi_luftdaten/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICSTORAGE = "Whitenoise.storage.CompressedManifestStaticFilesStorage" #zips up static files
 
