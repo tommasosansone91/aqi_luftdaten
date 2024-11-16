10bd92c1782ac7e6ab9165890faf8250c82bc669
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-11-16 17:52:22 +0100

    added command to clear series and realtime models

---
+++ pm_lookup/management/commands/clear_serie_and_realtime_models.py
@@ -0,0 +1,27 @@
+
+# usage
+#----------
+
+# python manage.py clear_models
+
+
+from django.core.management.base import BaseCommand
+from pm_lookup.models import \
+    RealtimeDatapoints, \
+    DatapointsSerie, \
+    DailyAggregatedDatapointsSerie, \
+    HourlyAggregatedDatapointsSerie
+
+"""
+This command is to delete all data in the series models.
+"""
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        RealtimeDatapoints.objects.all().delete()
+
+        # deleting the series(+graphs), not the historical dataponts
+        DatapointsSerie.objects.all().delete()
+        DailyAggregatedDatapointsSerie.objects.all().delete()
+        HourlyAggregatedDatapointsSerie.objects.all().delete()
