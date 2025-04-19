c9d1c93bbc84bd3d87de86e4f0ce6ef916efbfee
tommasosansone91 <tommaso.sansone91@virgilio.it>
2025-04-19 12:05:18 +0200

    prettifying

--- pm_lookup/processing/utils/air_quality_evaluators.py
+++ pm_lookup/processing/utils/air_quality_evaluators.py
@@ -6,33 +6,33 @@ def evaluate_PM10(PM10_value):
 
     # categorie di qualità dell'aria rispetto a PM 10
 
-    if PM10_value <=20:
-        PM10_mean_cathegory_label="Ottima"
-        PM10_mean_cathegory="prima"
+    if PM10_value <= 20:
+        PM10_mean_cathegory_label = "Ottima"
+        PM10_mean_cathegory = 1
 
-    elif PM10_value>=20 and PM10_value <=35:
-        PM10_mean_cathegory_label="Buona"
-        PM10_mean_cathegory="seconda"
+    elif PM10_value >= 20 and PM10_value  <= 35:
+        PM10_mean_cathegory_label = "Buona"
+        PM10_mean_cathegory = 2
     
-    elif PM10_value>=35 and PM10_value <=50:
-        PM10_mean_cathegory_label="Accettabile"
-        PM10_mean_cathegory="terza"
+    elif PM10_value >= 35 and PM10_value  <= 50:
+        PM10_mean_cathegory_label = "Accettabile"
+        PM10_mean_cathegory = 3
 
-    elif PM10_value>=50 and PM10_value <=100:
-        PM10_mean_cathegory_label="Fuori legge"
-        PM10_mean_cathegory="quarta"
+    elif PM10_value >= 50 and PM10_value  <= 100:
+        PM10_mean_cathegory_label = "Fuori legge"
+        PM10_mean_cathegory = 4
 
-    elif PM10_value>=100 and PM10_value <=200:
-        PM10_mean_cathegory_label="Pericolosa"
-        PM10_mean_cathegory="quinta"
+    elif PM10_value >= 100 and PM10_value  <= 200:
+        PM10_mean_cathegory_label = "Pericolosa"
+        PM10_mean_cathegory = 5
 
-    elif PM10_value>=200:
-        PM10_mean_cathegory_label="Emergenziale"
-        PM10_mean_cathegory="sesta"
+    elif PM10_value >= 200:
+        PM10_mean_cathegory_label = "Emergenziale"
+        PM10_mean_cathegory = 6
 
     else:
-        PM10_mean_cathegory_label="No data"
-        PM10_mean_cathegory="nessuna"
+        PM10_mean_cathegory_label = "No data"
+        PM10_mean_cathegory = None
 
     return (PM10_mean_cathegory_label, PM10_mean_cathegory)
 
@@ -42,33 +42,33 @@ def evaluate_PM25(PM25_value):
 
     # categorie di qualità dell'aria rispetto a PM 2.5
 
-    if PM25_value <=10:
-        PM25_mean_cathegory_label="Ottima"
-        PM25_mean_cathegory="prima"
+    if PM25_value <= 10:
+        PM25_mean_cathegory_label = "Ottima"
+        PM25_mean_cathegory = 1
 
-    elif PM25_value>=10 and PM25_value <=20:
-        PM25_mean_cathegory_label="Buona"
-        PM25_mean_cathegory="seconda"
+    elif PM25_value >= 10 and PM25_value  <= 20:
+        PM25_mean_cathegory_label = "Buona"
+        PM25_mean_cathegory = 2
     
-    elif PM25_value>=20 and PM25_value <=25:
-        PM25_mean_cathegory_label="Accettabile"
-        PM25_mean_cathegory="terza"
+    elif PM25_value >= 20 and PM25_value  <= 25:
+        PM25_mean_cathegory_label = "Accettabile"
+        PM25_mean_cathegory = 3
 
-    elif PM25_value>=25 and PM25_value <=50:
-        PM25_mean_cathegory_label="Fuori legge"
-        PM25_mean_cathegory="quarta"
+    elif PM25_value >= 25 and PM25_value  <= 50:
+        PM25_mean_cathegory_label = "Fuori legge"
+        PM25_mean_cathegory = 4
 
-    elif PM25_value>=50 and PM25_value <=100:
-        PM25_mean_cathegory_label="Pericolosa"
-        PM25_mean_cathegory="quinta"
+    elif PM25_value >= 50 and PM25_value  <= 100:
+        PM25_mean_cathegory_label = "Pericolosa"
+        PM25_mean_cathegory = 5
 
-    elif PM25_value>=100:
-        PM25_mean_cathegory_label="Emergenziale"
-        PM25_mean_cathegory="sesta"
+    elif PM25_value >= 100:
+        PM25_mean_cathegory_label = "Emergenziale"
+        PM25_mean_cathegory = 6
 
     else:
-        PM25_mean_cathegory_label="No_data"
-        PM25_mean_cathegory="nessuna"
+        PM25_mean_cathegory_label = "No_data"
+        PM25_mean_cathegory = None
 
     return (PM25_mean_cathegory_label, PM25_mean_cathegory)
 
