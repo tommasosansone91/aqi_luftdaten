819cb9a1c5ddc9c7f91d7f0920f6b009a294cf76
tommasosansone91 <tommaso.sansone91@virgilio.it>
2025-04-19 14:17:20 +0200

    going on developing a logic that build series and graph based on what is stated by series parameters

--- pm_lookup/processing/scheduled_processing_2.py
+++ pm_lookup/processing/scheduled_processing_2.py
@@ -5,22 +5,22 @@ from django.utils import timezone
 import numpy as np
 
 from pm_lookup.models import TargetArea
-from pm_lookup.models import RealtimeDatapoints
 from pm_lookup.models import HistoricalDatapoints
 from pm_lookup.models import DatapointsSerieParameters
 from pm_lookup.models import DatapointsSerieComputed
 
 # importo i drawers
-from pm_lookup.drawers.drawer1 import draw_timeserie_PM10_graph
-from pm_lookup.drawers.drawer1 import draw_timeserie_PM25_graph
+from pm_lookup.drawers.drawer1 import draw_timeserie_PM10_graph, draw_timeserie_PM25_graph
 
 # aggiunto per fixare il fatto che nei grafici è mostrato orario come se fosse in UTC
 # errore sopraggiunto dopo il reset del db?
-from pm_lookup.processing.utils.time_converters import fix_timezone_mismatch_1
+from pm_lookup.processing.utils.time_converters import fix_timezone_mismatch_in_array_of_datetimes
+
 from pm_lookup.processing.utils.air_quality_evaluators import evaluate_PM_in_HistoricalDatapoints_elements
 
 
-# this function must parse all the datapointsserieparameters and realize the correspondant serie for each of them.
+# this function must parse all the datapointsserieparameters 
+# and build the correspondant serie for each of them.
 
 
 def arrange_datapoints_series_and_graphs():
@@ -29,56 +29,74 @@ def arrange_datapoints_series_and_graphs():
 
     print("Eliminate tutte le serie storiche in DatapointsSerieComputed!")
 
-    print("Inizio disposizione dati in serie storiche per ogni località...")
+    print("Inizio disposizione dati ed elementi del grafico per ogni set di parametri definito...")
+
+    sets_of_serie_parameters = DatapointsSerieParameters.objects.all()
 
 
-    for area_di_interesse in TargetArea.objects.all():
+    for set_of_serie_parameters in sets_of_serie_parameters:
 
-        n_giorni = 30
+        # filter the historical datapoints based on the place and time horizon
+        #---------------------------------------------------------------------
 
-        print("Predisposizione dati ed elementi del grafico per la serie storica per %s..." % area_di_interesse.name)
+        print("Predisposizione dati ed elementi del grafico per la serie storica definita dal set di parametri %s..." % area_di_interesse.name)
+
+        area_di_interesse = set_of_serie_parameters.target_area
+        time_horizon = set_of_serie_parameters.time_horizon
 
         # isola i record di una località - è cmq un gruppo di oggetti
         # +
         # prendo i record delle 24 ore degli ultimi 30 giorni
         records_serie_storica = HistoricalDatapoints.objects.filter(
             Target_area_input_data = area_di_interesse,
-            Last_update_time__gte = timezone.now() - timedelta(days=n_giorni),
+            # Last_update_time__gte = timezone.now() - timedelta(days=n_giorni),
+            Last_update_time__gte = timezone.now() - time_horizon,
             Last_update_time__lte = timezone.now()
         )
 
-        # nota: i dati sno già ordinati per default in ordine decrescente
+        # nota: i dati sono già ordinati per default in ordine decrescente
 
         # evaluate pm mean values into cathegories to add them to the series
         results_dict = evaluate_PM_in_HistoricalDatapoints_elements(records_serie_storica)
 
         
-
         serie_storica = {
-                        #ce n'è solo una perchè l'ho filtrata
-                        "TargetArea" : area_di_interesse.name,
+            #ce n'è solo una perchè l'ho filtrata
+            "TargetArea" : area_di_interesse.name,
 
-                        # questi sono vettori di valori
+            # questi sono vettori di valori
 
-                        "last_update_time_values" : [i.last_update_time for i in records_serie_storica],
+            "last_update_time_values" : [i.last_update_time for i in records_serie_storica],
 
-                        "PM10_mean_values" : [i.PM10_mean for i in records_serie_storica],
-                        "PM25_mean_values" : [i.PM25_mean for i in records_serie_storica],
+            "PM10_mean_values" : [i.PM10_mean for i in records_serie_storica],
+            "PM25_mean_values" : [i.PM25_mean for i in records_serie_storica],
 
-                        "PM10_mean_cathegory_label_values" : results_dict["PM10_mean_cathegory_label_records_serie_storica"],
-                        "PM25_mean_cathegory_label_values" : results_dict["PM25_mean_cathegory_label_records_serie_storica"],
+            "PM10_mean_cathegory_label_values" : results_dict["PM10_mean_cathegory_label_records_serie_storica"],
+            "PM25_mean_cathegory_label_values" : results_dict["PM25_mean_cathegory_label_records_serie_storica"],
 
-                        "PM10_mean_cathegory_values" : results_dict["PM10_mean_cathegory_records_serie_storica"],
-                        "PM25_mean_cathegory_values" : results_dict["PM25_mean_cathegory_records_serie_storica"],
+            "PM10_mean_cathegory_values" : results_dict["PM10_mean_cathegory_records_serie_storica"],
+            "PM25_mean_cathegory_values" : results_dict["PM25_mean_cathegory_records_serie_storica"],
 
-                        "number_of_contributing_sensors_values" : [i.number_of_contributing_sensors for i in records_serie_storica],
+            "number_of_contributing_sensors_values" : [i.number_of_contributing_sensors for i in records_serie_storica],
 
-                        }
+        }
 
 
         # aggiunto per fixare il fatto che nei grafici è mostrato orario come se fosse in UTC
         # errore sopraggiunto dopo il reset del db?
-        serie_storica["last_update_time_values"] = fix_timezone_mismatch_1(serie_storica["last_update_time_values"])
+        serie_storica["last_update_time_values"] = fix_timezone_mismatch_in_array_of_datetimes(serie_storica["last_update_time_values"])
+
+
+        # group elements of the serie into group of data 
+        # that must have their values turned to a mean or other statistics
+        #---------------------------------------------------------------------
+
+        # scorrere time horizon per aggregation period, e continuare il cliclo finchè il rimasto di time horizon non è <=0
+        
+
+        # ciclo for che
+
+
 
 
         # la posizione di serie storiche indica la città
@@ -92,7 +110,7 @@ def arrange_datapoints_series_and_graphs():
         PM10_values = np.array(serie_storica['PM10_mean_values'])
         PM25_values = np.array(serie_storica['PM25_mean_values'])  
 
-            # colora il retro del grafico per fasce anzchè fare le linee di soglia
+        # colora il retro del grafico per fasce anzchè fare le linee di soglia
 
 
         # questo script è orario, non servono i limiti normativi
