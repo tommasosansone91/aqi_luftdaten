bdf5590b3f28f1aab5f4dfac88623091a8132c50
tommasosansone91 <tommaso.sansone91@virgilio.it>
2025-05-13 16:02:28 +0200

    added base urls for map search engines

--- pm_lookup/configs/constants.py
+++ pm_lookup/configs/constants.py
@@ -4,6 +4,9 @@
 
 KILOMETERS_TO_COORDINATES_POINTS_DISTANCE: float = 0.011300045235255235
 
+TEMPLATE_URL_OF_MAP_SEARCH_ENGINE = "https://www.google.com/search?q={latitude}, {longitude}"
+# TEMPLATE_URL_OF_MAP_SEARCH_ENGINE = "https://www.openstreetmap.org/#map=11/{latitude}/{longitude}"
+
 ALL_SENSORS_DATA_URL = "https://data.sensor.community/static/v2/data.json"
  # https://data.sensor.community/static/v2/data.1h.json dati 1h
 # https://data.sensor.community/static/v2/data.json dati 5 min
