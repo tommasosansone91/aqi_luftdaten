37aa781df7e21b152add7d45e5174f78c389565d
tommasosansone91 <tommaso.sansone91@virgilio.it>
2025-06-10 11:33:24 +0200

    - moved asynchronous functions of model SerieParametersSet into an external auxiliary file. - the components are inside the class AsynchronousComponentsToolbox1 - the class AsynchronousComponentsToolbox1 is instantiated inside model  SerieParametersSet. . the components are called inside the model's functions as self.toolbox_instance_1.component() .

--- pm_lookup/models.py
+++ pm_lookup/models.py
@@ -6,16 +6,12 @@ from django.utils import timezone
 
 import uuid
 
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from pm_lookup.configs.constants import COMPUTED_SERIE_META_VERBOSE_NAME, SET_OF_PARAMETERS_OF_SERIE_META_VERBOSE_NAME
 
-from pm_lookup.processing.model_update_triggered_processing_1 import content_of_generate_series_and_draw_graphs
-
+from pm_lookup.processing.asynchronous_components_1 import AsynchronousComponentsToolbox1
 
 
 # Create your models here.
@@ -267,52 +263,27 @@ class SerieParametersSet(models.Model):
     
     # default: create a time serie of 1h aggregation and having a 1-day time horizon
 
+    #----------------------------
     # asynchronous running block
     #----------------------------
 
-    def generate_series_and_draw_graphs(self):
-
-        # Create a thread pool executor.
-        # max_workers=1 -> it's set up to manage one thread at a time for executing tasks asynchronously.
-        executor_1 = ThreadPoolExecutor(max_workers=1)
-
-        # create a new loop.
-        # it manages the execution of asynchronous tasks.
-        loop_1 = asyncio.new_event_loop()
-
-        # set the newly created event loop as the current event loop.
-        asyncio.set_event_loop(loop_1)
-
-        # Run the async function in the (context of) new event loop, by using the thread pool executor.
-        # this offloads the execution of self._run_async to the executor, making it run asynchronously without blocking the main thread.
-        loop_1.run_in_executor(executor_1, self._run_async)
-
-    def _run_async(self):
-        # use asyncio.run() method to run a function that was defined as async.
-        # asyncio.run() is used to run coroutines.
-        # It handles the event loop management for you, which means it creates a new event loop, runs the coroutine until it completes, and then closes the loop. This is simpler than managing the event loop manually, making it a convenient option for running a single asynchronous function.
-        asyncio.run(self.generate_series_and_draw_graphs_async())
-
-    async def generate_series_and_draw_graphs_async(self):
-        # when this function is called (without any command before its name), it returns a coroutine
-
-        # execute this coroutine and give me its return value.
-        # await is legal only inside an async function.
-        await content_of_generate_series_and_draw_graphs()
-
+    model_update_processing_1 = AsynchronousComponentsToolbox1()
+    print("Generated instance model_update_processing_1 of class AsynchronousComponentsToolbox1!")
 
     #----------------------------
 
 
     def save(self, *args, **kwargs):
+
         # First, save the instance
         super().save(*args, **kwargs) 
         print("Changes in model have been saved!") 
-        print("Triggering the rebuilding and redrawing of the corresponding series!")
-        
-        # Then rebuild series and graphs
-        self.generate_series_and_draw_graphs() 
 
+        # Then rebuild series and graphs
+        print("Triggering the rebuilding and redrawing of the corresponding series!")
+        self.model_update_processing_1.generate_series_and_draw_graphs() 
+
+        print("Triggered the rebuilding and redrawing of the corresponding series!")
         print("Wait for the serie rebuilding and redrawing to finish...") 
 
 
@@ -336,7 +307,8 @@ class SerieParametersSet(models.Model):
 # Using Django's post_save signal
 @receiver(post_save, sender=SerieParametersSet)
 def trigger_async_generation(sender, instance, **kwargs):
-    instance.generate_series_and_draw_graphs()
+    instance.model_update_processing_1.generate_series_and_draw_graphs() 
+
 
 
 class ComputedSerie(models.Model):
