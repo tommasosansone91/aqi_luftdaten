ba02d1021424ddbc02faef9fd200ba68e0e25681
tommasosansone91 <tommaso.sansone91@virgilio.it>
2025-06-10 10:50:08 +0200

    changed function order, varialbes names, added comments

--- pm_lookup/models.py
+++ pm_lookup/models.py
@@ -265,23 +265,44 @@ class SerieParametersSet(models.Model):
         ) 
     
     
-
     # default: create a time serie of 1h aggregation and having a 1-day time horizon
 
-    async def generate_series_and_draw_graphs_async(self):
-        await content_of_generate_series_and_draw_graphs()
+    # asynchronous running block
+    #----------------------------
 
     def generate_series_and_draw_graphs(self):
-        # Create a thread pool executor
-        executor = ThreadPoolExecutor(max_workers=1)
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
 
-        # Run the async function in the new event loop
-        loop.run_in_executor(executor, self._run_async)
+        # Create a thread pool executor.
+        # max_workers=1 -> it's set up to manage one thread at a time for executing tasks asynchronously.
+        executor_1 = ThreadPoolExecutor(max_workers=1)
+
+        # create a new loop.
+        # it manages the execution of asynchronous tasks.
+        loop_1 = asyncio.new_event_loop()
+
+        # set the newly created event loop as the current event loop.
+        asyncio.set_event_loop(loop_1)
+
+        # Run the async function in the (context of) new event loop, by using the thread pool executor.
+        # this offloads the execution of self._run_async to the executor, making it run asynchronously without blocking the main thread.
+        loop_1.run_in_executor(executor_1, self._run_async)
 
     def _run_async(self):
+        # use asyncio.run() method to run a function that was defined as async.
+        # asyncio.run() is used to run coroutines.
+        # It handles the event loop management for you, which means it creates a new event loop, runs the coroutine until it completes, and then closes the loop. This is simpler than managing the event loop manually, making it a convenient option for running a single asynchronous function.
         asyncio.run(self.generate_series_and_draw_graphs_async())
+
+    async def generate_series_and_draw_graphs_async(self):
+        # when this function is called (without any command before its name), it returns a coroutine
+
+        # execute this coroutine and give me its return value.
+        # await is legal only inside an async function.
+        await content_of_generate_series_and_draw_graphs()
+
+
+    #----------------------------
+
 
     def save(self, *args, **kwargs):
         # First, save the instance
