c94f1c6ec6aa6ef9f900181a973c7ab56ca60a72
tommasosansone91 <tommaso.sansone91@virgilio.it>
2025-06-12 00:53:42 +0200

    fixed calling of generate_series_and_draw_graphs()

--- pm_lookup/management/commands/generate_series_and_draw_graphs.py
+++ pm_lookup/management/commands/generate_series_and_draw_graphs.py
@@ -6,8 +6,7 @@
 
 from django.core.management.base import BaseCommand
 
-# from pm_lookup.processing.scheduled_processing_2 import generate_series_and_draw_graphs
-from pm_lookup.models import SerieParametersSet
+from pm_lookup.processing.asynchronous_components_1 import AsynchronousComponentsToolbox1
 
 """
 This command is to recreate the time series 
@@ -20,6 +19,15 @@ class Command(BaseCommand):
 
         #arrangia le serie storiche attingendo al modello storico grezzo e ridisegna i grafici
 
-        serie_parameters_set_model = SerieParametersSet()
+        print("BaseCommand generate_series_and_draw_graphs - S")
 
-        serie_parameters_set_model.generate_series_and_draw_graphs()
+        model_update_processing_1 = AsynchronousComponentsToolbox1()
+        print("Generated instance model_update_processing_1 of class AsynchronousComponentsToolbox1!")
+
+        print("Triggering the rebuilding and redrawing of the corresponding series!")
+        model_update_processing_1.generate_series_and_draw_graphs() 
+
+        print("Triggered the rebuilding and redrawing of the corresponding series!")
+        print("Wait for the serie rebuilding and redrawing to finish...") 
+
+        print("BaseCommand generate_series_and_draw_graphs- E")
